51206aa6b1eac14c7c02249ba83335841dac507f
lama <dimasi73378@gmail.com>
2026-05-24 13:20:26 +0600

    лаб 6

--- BitString.py
+++ BitString.py
@@ -2,28 +2,33 @@ import sys
 
 class BitString:
     def __init__(self, inp_string=""):
-        self.bs = ['0'] * 8
+        self.size = 8
+        self.bs = ['0'] * self.size
 
         if inp_string:
-            tmps = ['0'] * 8
+            tmps = ['0'] * self.size
             i = 0
-            while i < 8 and i < len(inp_string):
+            length = len(inp_string) if len(inp_string) < self.size else self.size
+
+            while i < length:
                 c = inp_string[i]
                 if c != '0' and c != '1':
-                    print("Некорректный символ в строке")
+                    print(f"Некорректный символ в строке: {c}")
                     sys.exit(0)
                 tmps[i] = c
                 i += 1
+
             for j in range(i):
-                self.bs[8 - i + j] = tmps[j]
+                self.bs[self.size - i + j] = tmps[j]
 
     def input(self):
-        tmps = ['0'] * 8
+        tmps = ['0'] * self.size
         i = 0
-        print("Введите битовую строку: ", end="")
+
+        print("Введите битовую строку : ", end="")
         user_line = sys.stdin.readline()
         for tmp in user_line:
-            if i >= 8 or tmp == '\n':
+            if i >= self.size or tmp == '\n':
                 break
             if tmp != '0' and tmp != '1':
                 print("Некорректный ввод")
@@ -31,20 +36,23 @@ class BitString:
             tmps[i] = tmp
             i += 1
 
-        self.bs = ['0'] * 8
+        self.bs = ['0'] * self.size
         for j in range(i):
-            self.bs[8 - i + j] = tmps[j]
+            self.bs[self.size - i + j] = tmps[j]
 
     def output(self):
-        for j in range(8):
+        for j in range(self.size):
             print(self.bs[j], end="")
         print()
 
-    def conjanction(self, b):  # сохранено оригинальное название
-        tmps = []
-        for j in range(8):
-            tmps.append('1' if (self.bs[j] == '1' and b.bs[j] == '1') else '0')
-        return BitString("".join(tmps))
+    def conjanction(self, b):
+        result = BitString()
+        for j in range(self.size):
+            if (self.bs[j] == '1') and (b.bs[j] == '1'):
+                result.bs[j] = '1'
+            else:
+                result.bs[j] = '0'
+        return result
 
     def fileinput(self, filename):
         try:
@@ -54,20 +62,20 @@ class BitString:
             print(f"Ошибка: файл {filename} не открыт")
             sys.exit(0)
 
-        tmps = ['0'] * 8
+        tmps = ['0'] * self.size
         i = 0
         for tmp in content:
-            if i >= 8:
+            if i >= self.size:
                 break
             if tmp == '0' or tmp == '1':
                 tmps[i] = tmp
                 i += 1
 
-        self.bs = ['0'] * 8
+        self.bs = ['0'] * self.size
         for j in range(i):
-            self.bs[8 - i + j] = tmps[j]
+            self.bs[self.size - i + j] = tmps[j]
 
     def fileoutput(self, filename):
         with open(filename, 'w', encoding='utf-8') as file:
-            for j in range(8):
+            for j in range(self.size):
                 file.write(self.bs[j])
--- lab python.py
+++ lab python.py
@@ -1,24 +1,54 @@
 import sys
-import locale
 from BitString import BitString
 
-def main():
-    a = BitString()
-    b = BitString()
-    c = BitString()
 
-    a.input()
-    print("Чтение строки 2 из файла b.txt...")
-    b.fileinput("b.txt")
-    print("Строка 1: ", end="")
+def main():
+    print("проверка конкструкторов")
+
+    print("\n1. Конструктор по умолчанию:")
+    a = BitString()
+    print("a = ", end="")
     a.output()
-    print("Строка 2: ", end="")
+
+    print("\n2. Конструктор с параметром:")
+    b = BitString("1010")
+    print("b = ", end="")
     b.output()
-    c = a.conjanction(b)
-    print("Результат (AND): ", end="")
+
+    print("\n3. Конструктор копирования:")
+    c = BitString()
+    c = BitString("".join(b.bs))
+    print("c (копия b) = ", end="")
     c.output()
-    print("Сохранение в c.txt...")
-    c.fileoutput("c.txt")
+
+    print("\n4. Ввод строки 1 с консоли:")
+    d = BitString()
+    d.input()
+    print("d = ", end="")
+    d.output()
+
+    print("\n5. Чтение строки 2 из файла b.txt:")
+    e = BitString()
+    e.fileinput("b.txt")
+    print("e = ", end="")
+    e.output()
+
+    print("\n6. Конъюнкция d AND e:")
+    f = d.conjanction(e)
+    print("f = ", end="")
+    f.output()
+
+    print("\n7. Сохранение в c.txt...")
+    f.fileoutput("c.txt")
+    print("Готово!")
+
+    print("\n8. Оператор присваивания:")
+    g = BitString()
+    g = f
+    print("g = f = ", end="")
+    g.output()
+    input("Нажмите Enter для выхода...")
+
 
 if __name__ == "__main__":
     main()
